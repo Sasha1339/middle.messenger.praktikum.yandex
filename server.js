0c5ded03914ca9781e426a212101e8c4f3c251ed
Sesh <sergeimorozov212001@gmail.com>
2025-02-07 17:33:53 +0300

    fix()

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Настроить статичное обслуживание файлов из папки build (или dist)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
