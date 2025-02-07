28d754f803feb3223daf85cee5f49797ff79b6f8
Sesh <sergeimorozov212001@gmail.com>
2025-02-07 13:19:54 +0300

    fix()

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import {defineConfig} from 'vite'
 import handlebars from "vite-plugin-handlebars";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
     root: '.',
     build: {
         outDir: 'dist',
     },
-    plugins: [handlebars(), nodePolyfills()],
+    plugins: [handlebars()],
 })
