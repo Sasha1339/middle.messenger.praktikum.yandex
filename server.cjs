483c7f9df626cfcc1edc9c52af8ac3484038b8d3
Sesh <sergeimorozov212001@gmail.com>
2025-02-07 19:17:12 +0300

    fix()

--- server.cjs
+++ server.cjs
@@ -1,5 +1,5 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +8,7 @@ app.use(express.static('./dist'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
 
-    res.status(200)
+    res.status(200);
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
