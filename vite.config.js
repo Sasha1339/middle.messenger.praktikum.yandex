360ff08abdde96a9f45228a4c85a4773114a8925
Sesh <sergeimorozov212001@gmail.com>
2025-02-07 19:32:50 +0300

    fix()

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import {defineConfig} from 'vite'
 import { resolve } from 'path';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-require('crypto-browserify');
 
 export default defineConfig({
-    define: {
-        'global': 'globalThis',
-    },
     build: {
         rollupOptions: {
             input: {
