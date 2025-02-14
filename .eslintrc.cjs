cf51e377a43e49d7e509522f4251987b1ce35a4e
Sesh <sergeimorozov212001@gmail.com>
2025-02-14 23:53:10 +0300

    fix(tsconf)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,24 +9,9 @@ module.exports = {
         project: ["tsconfig.json"]
       },
       extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:prettier/recommended"
       ],
       rules: {
-        "react/jsx-filename-extension": "off",
-        "import/extensions": "off",
-        "import/no-extraneous-dependencies": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/ban-ts-comment": "error",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-this-alias": "off",
-        "@typescript-eslint/prefer-promise-reject-errors": "off",
         "prettier/prettier": [
           "error",
           {
